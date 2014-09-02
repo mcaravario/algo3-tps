b1af687606db53dcd5f2b7c8435268b73ebba682
fhosen <fhosen@hotmail.com>
2014-09-02 14:55:46 -0300

    testeando funciomiento del 2

--- tp1/ej2/ejercicio2.cpp
+++ tp1/ej2/ejercicio2.cpp
@@ -1,5 +1,3 @@
-
-
 #include "ejercicio2.h"
 #include <iostream>
 #include <set>
@@ -7,71 +5,42 @@
 using namespace std;
 
 
+void mostrar_edificio(edificio ed){
+	cout << "[ (" << ed.v1.x << "," << ed.v1.y << ") (" << ed.v2.x << "," << ed.v2.y << ") ]   "; 
+}
+
 int main(int argc, char** argv){
+	
 
-	int cant_edificios = atoi(argv[1]);
+		int cant_edificios = atoi(argv[1]);
+		list<vertice> res;
+		set<edificio> edificios;
+	
+		for(int i=0; i<cant_edificios; i++){
+
+			int izq = atoi(argv[2+i*3]);
+			int alt = atoi(argv[2+i*3+1]);
+			int der = atoi(argv[2+i*3+2]); 
  
-	list<vertice> res;
+			edificio ed = edificio(izq,alt,der); 
+			edificios.insert(ed);
+		}
+		set<edificio>::iterator its = edificios.begin();
+		while(its != edificios.end()){
+			mostrar_edificio(*its);
+			its++; 
+		}
+		cout << endl;
 
-	bool (*fn_pt)(edificio,edificio) = comparar_edificios;
-
-	set<edificio,bool (*)(edificio,edificio)> edificios(fn_pt);
 	
-	for(int i=0; i<cant_edificios; i++){
-
-		int izq = atoi(argv[2+i*3]);
-
-		int alt = atoi(argv[2+i*3+1]);
-   
-		int der = atoi(argv[2+i*3+2]); 
-  
-		vertice v1;
-
-		vertice v2;
-
-		v1.x = izq;
-		v1.y = alt;
-		v2.x = der;
-		v2.y = alt;
-
-		edificio ed;
-
-		ed.v1 = v1;
-		ed.v2 = v2;
-
-		edificios.insert(ed);
-	}
-	set<edificio>::iterator its = edificios.begin();
-	while (its != edificios.end()){
-		cout << "(" << its->v1.x << "," << its->v1.y << ")  (" << its->v2.x << "," << its->v2.y << ")" << endl;
-		its++;	
-	}
-	
-	//~ edificio e1 = edificio(1,4,3);
-	//~ edificio e2 = edificio(4,5,6);
-	//~ 
-	//~ cout << "(" << e1.v1.x << "," << e1.v1.y << ")  (" << e1.v2.x << "," << e1.v2.y << ")" << endl;
-	//~ cout << "(" << e2.v1.x << "," << e2.v1.y << ")  (" << e2.v2.x << "," << e2.v2.y << ")" << endl;
-	//~ 
-	//~ cout << comparar_edificios(e1,e2) << endl;
-	//~ cout << comparar_edificios(e2,e1) << endl;
-	 	//~ 
-	// Al salir del for tengo todos los edificios insertados ordenados en el avl
-
+		res = generar_horizonte(edificios);
+		list<vertice>::iterator it = res.begin();
 		
-
+		while(it != res.end()){
+		cout << "(" <<  it->x << "," << it->y << ")" << endl;
+		it++; 
+		}
+		
+		
 	return 0;
 }
-	
-	
-
-
-
-
-
-
-
-
-
-
-
--- tp1/ej2/ejercicio2.h
+++ tp1/ej2/ejercicio2.h
@@ -1,4 +1,3 @@
-
 #include <set>
 #include <list>
 #include <iterator>   
@@ -7,178 +6,98 @@
 
 using namespace std;
 
-
-
 struct vertice{
-  vertice();
-  vertice(int x, int y);	
+  vertice(int x, int y);
   int x;
   int y;
-  	
+	vertice() : x(0), y(0){}	
 };
 
 
 struct edificio{
-   edificio();
-   edificio(int x1, int y, int x2);		
-   vertice v1;
-   vertice v2;	
+	edificio(int x1, int y, int x2);
+	vertice v1;
+	vertice v2;	
 
+	bool operator<(edificio other) const {
+		return ((this->v1.x < other.v1.x) || ((this->v1.x == other.v1.x) && (this->v1.y > other.v1.y)) ||  ((this->v1.x == other.v1.x) && (this->v1.y == other.v1.y) && (this->v2.x < other.v2.x)) );
+	}
+
+	edificio() : v1(vertice()), v2(vertice()){}		
 };
 
-vertice::vertice(){
-	x=0;
-	y=0;
-}
 
 vertice::vertice(int a, int b){
 	x = a;
 	y = b;	
 }
 
+
 edificio::edificio(int x1, int y, int x2){
 	v1 = vertice(x1,y);
 	v2 = vertice(x2,y);	
 }
 
-edificio::edificio(){
-	v1.x = 0;
-	v1.y = 0;
-	v2.x = 0;
-	v2.y = 0;
-	
+
+bool hay_edificio_relacionado(set<edificio>::iterator it, set<edificio> edificios){
+
+	bool res;	
+	if (it == edificios.end()){
+		res = false;
+	} else {
+		set<edificio>::iterator sig = it++;
+		res = sig->v1.x <= it->v2.x;
 	}
 
-
-
-
-bool hay_x_edificios_relacionados(set<edificio>::iterator it, int cant, set<edificio> edificios){
-
-  bool parar = 0;                                        //Utilizo el flag para parar cuando encuentro los x edificios relacionados
-
-	edificio e = *it;
-  
-	bool res = true;
-
-  while(it != edificios.end() && !parar && cant > 0){
-
-		set<edificio>::iterator sig = it++;
-
-    if(!(sig->v1.x <= e.v2.x)){  	// Caso en el quei no hay un edificio relacionado  
-
-      parar = 1;
-      res = false;	
-
-    }else{
-
-      cant--;    
-      it++;
-	
-    }
-
-
- }
-
-return res;
-
+	return res;
 }
+
 
 void procesar_edificio_simple(set<edificio>::iterator it, list<vertice> res){
 
-
-	res.push_back(it->v1);		//agrego el primer vertice sin modificar
-	
-	vertice insertar = it->v2;						//modifico la altura del segundo vertice y la "bajo" al piso y luego la agrego al resultado
-
-	insertar.y = 0;
-	
+	res.push_back(it->v1);				//agrego el primer vertice sin modificar
+	vertice insertar = vertice(it->v2.x,0);		//modifico la altura del segundo vertice y la "bajo" al piso y luego la agrego al resultado
 	res.push_back(insertar);		
-
 }
 
 
-
-/********ACORDARSE DE HACER MACROS PARA COMPARAR LOS VERTICES **********/
-/*
-bool contengo_edificio(const iterador it){
-
-edificio e1 = it.actual();		
-edificio e2 = it.siguiente();		//Solo chequeo el edificio actual y el siguiente, ya que estan ordenados.
-
-bool res = false;
-
-  if((e1.v1 <= e2.v1) && (e2.v1 < e1.v2) && (e2.v2 <= e1.v2) && (e1.v1.y >= e2.v1.y) ){
-
-    res = true;
-  }		
-
-return res;
-
-}
-*/
-/*
-bool rompe_techo(edificio e, edificio sig){
-
-
-}
-
-bool contengo_vertice(edificio e, vertice v){
-
-}
-*/
-
-
-bool edificio_contenido(set<edificio>::iterator it, set<edificio>::iterator sig){
-
-return true;
-}
-
-
-void reordenar_estructura(set<edificio> edificios, set<edificio>::iterator it){
-}
-
-bool rompe_techo(set<edificio>::iterator it,set<edificio>::iterator sig){
-}
-
-bool comparar_edificios(edificio e1, edificio e2){
- 
-	return ((e1.v1.x < e2.v1.x) || ((e1.v1.x == e2.v1.x) && (e1.v1.y > e2.v1.y)) ||  ((e1.v1.x == e2.v1.x) && (e1.v1.y == e2.v1.y) && (e1.v2.x < e2.v2.x)) );
-
-}
-
-
-bool vertice_contenido(set<edificio>::iterator it, vertice v){
-	
+bool vertice_contenido(set<edificio>::iterator it, vertice v){	
 	return ((v.x < it->v2.x) && (v.x > it->v1.x) && (v.y < it->v1.y));
 } 
 
+bool edificio_contenido(set<edificio>::iterator it, set<edificio>::iterator sig){
+	return vertice_contenido(it,sig->v1) && vertice_contenido(it,sig->v2);
+}
+
+
+bool rompe_techo(set<edificio>::iterator it,set<edificio>::iterator sig){
+	// Por el invariante de la estructura, it->v1.x >= sig->.x, y si los x de ambos
+	// edificios coinciden la altura de E1 tiene que superar la de E2
+	return (sig->v2.x < it->v2.x) &&  (sig->v2.y > it->v2.y);
+}
+
+
+bool igual_altura(set<edificio>::iterator it1, set<edificio>::iterator it2){
+	return (it1->v1.y == it2->v1.y);
+}
 
 
 list<vertice> generar_horizonte(set<edificio>& edificios){
 
-list<vertice>  res;
-
-set<edificio>::iterator it = edificios.begin();
-
-
+	list<vertice>  res;
+	set<edificio>::iterator it = edificios.begin();
 
 	while(it!= edificios.end()){
   
-		if(hay_x_edificios_relacionados(it,0,edificios)){         //Si no hay edificios relacionados
-		
-
+  		if(!hay_edificio_relacionado(it,edificios)){        //Si no hay edificios relacionados
 			procesar_edificio_simple(it,res);
 			it++;
-
 		}else{            //Caso en el que se relacionan 2 edificios o mas
-		
 			set<edificio>::iterator sig = it++;
-			
 			if(edificio_contenido(it,sig)){
-
 				edificios.erase(sig);
-			
-				if(hay_x_edificios_relacionados(it,0,edificios)){
+	
+				if(!hay_edificio_relacionado(it,edificios)){
 					procesar_edificio_simple(it,res);
 					it++;
 				}
@@ -199,30 +118,38 @@ set<edificio>::iterator it = edificios.begin();
 					it++;
 				
 				}else{
-					/*
+					
 					if(vertice_contenido(it,sig->v1)){
 						//creo nuevo edificio con el vertice corrido de lugar
 						edificio nuevo_edificio = edificio(it->v2.x,sig->v1.y,sig->v2.x);
 						//elimino el siguiente
-						edicios.erase(sig);
+						edificios.erase(sig);
 						//inserto ordenadamente el nuevo edificio que cree
 						edificios.insert(nuevo_edificio);
 						
-						if(hay_x_edificios_relacionados(it,0,edificios){
+						if(!hay_edificio_relacionado(it,edificios)){
 						
 							res.push_back(it->v1);
 							it++;
 						}
-					*/	
 						
-					}	
-					
+					}else{
+						if(vertice_contenido(sig,it->v2)){
+							res.push_back(it->v1);
+							it++;
+						}else{
+							if(igual_altura(it,sig)){
+								// Creo un edificio que es la "union" de ambos, reemplazando a e1 y e2.
+								edificio nuevo_ed = edificio(it->v1.x,it->v1.y,sig->v2.x);
+								edificios.erase(sig);
+								edificios.insert(nuevo_ed);
+								it++;
+							}	
+						}
+					}		
 				}			
 			}					
-		}                                                      
-	
+		}	                                                      
+	}
 	return res;
 }
-
-
-
