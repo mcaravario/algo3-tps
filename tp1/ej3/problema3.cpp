e6426372aeae8a9fa31a97bfff58dc4c3cd69fd3
Flower <flower@Power.(none)>
2014-09-04 22:06:12 -0300

    ej3

--- tp1/ej3/problema3.cpp
+++ tp1/ej3/problema3.cpp
@@ -6,7 +6,6 @@ int main(int argc,char* argv[]){
 	int umbral;
 	while(getline(cin, linea)){
 		if(linea.size()>2){
-		cout << "LINEA: " << linea << endl;
 		elems = tomar_elementos(linea);
 		umbral = tomar_umbral(linea);
 		resultado vacio = resultado();
@@ -22,21 +21,12 @@ int main(int argc,char* argv[]){
 				matriz[i].resize(n);
 				scanf("%i",&rango);
 				matriz[i][j]=rango;
-				cout << matriz[i][j];
 				j++;
 			}
 			lista_productos.push_back(i+1);
-      cout << endl;
       i++;
 		}
 		lista_productos.push_back(i+1);
-		cout << "LISTA DE PRODUCTOS: ";
-		list<producto>::iterator itl = lista_productos.begin();
-		while(itl != lista_productos.end()){
-			cout <<*itl<< " ";
-			itl++;
-		}
-		cout<< endl;
 		//peor solucion
 		resultado res_g = resultado();
 		res_g.cant = elems;
