65bf998cc650d053b125973cd5242ed206948c1c
Maria Florencia Lagos amartino <flagos@ws5.labo4>
2014-09-14 18:11:00 -0300

    ej1 test_finished

--- tp1/ej1/testear.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-# PARAMS: cantidad de tablones ini,cantidad de tablones hasta,tablones incremento, capacidad de salto, cantidad de iteraciones
-typeset -i i END
-let i=$1 END=$2 INC=$3
-
-while ((i<END)); do
-	./gen $4 $i | ./ej1test $5
-	i=$i+$INC
-done
