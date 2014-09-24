e6f5aa4a3f91b3a3d0a1d94f9e50fb35751996c6
Martin Caravario <mcaravario@ws5.labo7>
2014-09-24 17:06:21 -0300

    Terminado generador de instancias aleatorias del ejercicio 2

---
+++ tp1/ej2/test/generador_promedio.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+#include <string>
+#include "ejercicio2.h"
+
+using namespace std;
+
+// La funcion recibe la cantidad de edificios a crear y genera una instancia valida aleatoria.
+int main(int argc, char** argv){
+	int cant_ed;
+        cant_ed = atoi(argv[1]);
+	cout << cant_ed << " ";
+	for (int i = 0; i < cant_ed; i++){
+		int x1 = rand() % 100;
+		int x2 = rand() % 50;
+		int y = rand() % 200;
+		if(x2 < x1 ){
+                        int c = x2;
+			x2 = x1;
+                        x1 = c;
+		}		
+		
+		if(y == 0){
+			y = rand() % 15;
+		}
+		
+	cout << x1 << " " << y << " " << x2;
+        if(i!=cant_ed-1)
+          cout << " ";
+      }
+      return 0;
+
+}
