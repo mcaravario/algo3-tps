0f0f4ee3cd3af9cf2e2d5ea840107764dcdb0d6a
mcaravario <mcaravario@dc.uba.ar>
2014-10-02 15:52:41 -0300

    agregada la nueva poda al ejercicio 3

---
+++ tp1/tests/ej3/podas/poda_piola/poda_peligrosa.cpp
@@ -0,0 +1,12 @@
+
+/* Todos los productos que no pueden compartir camion con los demás, van solos en camiones propios.
+ **/
+void poda_ultra_peligrosos(resultado& res_parcial, list<producto>& Q, vector<vector<coef> > mz, unsigned int umbral, resultado& res_g){
+
+	auto it = Q.begin();
+	while (it != Q.end()){
+		auto 
+	
+	}
+
+}
