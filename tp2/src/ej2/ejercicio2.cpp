411367a269b2f6135f727bac51f957f798bdbe69
mcaravario <mcaravario@dc.uba.ar>
2014-10-15 12:46:29 -0300

    Avanzando sobre el ejercicio 2, quedan algunos errores y la funcion calcular posiciones

--- tp2/src/ej2/ejercicio2.cpp
+++ tp2/src/ej2/ejercicio2.cpp
@@ -5,6 +5,63 @@
 
 using namespace std;
 
+/*Esta funcion devuelve true si no hay solucion y false en caso contrario */ 
+bool no_hay_sol(int** matriz, int n){
+	for(int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			if(matriz[i][j] == -1){
+				return true;
+			}
+		}
+	}
+
+return false;
+}
+
+
+/* Esta funcion modifica la primer matriz, dejando la suma de todas las
+matrices pasadas como parametro */
+void sumar_matrices(int** matriz_res, int*** matrices_caballos, int n, int k){
+	
+	//inicializo el resultado en 0
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			matriz_res[i][j] = 0;		
+		}
+	}
+	/* 
+	 Sumo las matrices considerando si alguno de los valores a sumar
+	 es -1, si es asi dejo -1, sino los sumo.
+	*/
+	for(int h = 0; h<k ; h++){
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(matrices_caballos[h][i][j] == -1 || matriz_res[i][j] == -1){
+					matriz_res[i][j] = -1;
+				} else {
+					matriz_res[i][j] = matriz_res[i][j] + matrices_caballos[h][i][j];
+				}
+			}
+		}
+	}
+}
+
+tuple<int,int,int> buscar_minimo(int** matriz, int n){
+	/*inicializo el resultado con el valor del primer casillero de
+	 * la matriz*/
+	tuple<int,int,int> res(matriz[0][0],0,0) ;
+	for(int i = 0; i<n; i++){
+		for (int j = 0; j<n ; j++){
+			if(get<0>(res) > matriz[i][j]){
+				tuple<int,int,int> nuevo_min(matriz[i][j],i,j);
+				res = nuevo_min;
+			}
+		}
+	}
+return res;
+
+}
+
 // Esta funcion devuelve una terna con m f c.
 tuple<int, int, int> caballos_salvajes(int n, int k, pair<int, int> posiciones_caballos[]){
 
@@ -30,7 +87,7 @@ tuple<int, int, int> caballos_salvajes(int n, int k, pair<int, int> posiciones_c
 	}
 	
 	int matriz_res[n][n];
-	sumar_matrices(matriz_res, matrices_caballos);
+	sumar_matrices(matriz_res, matrices_caballos,n,k);
 
 	if(no_hay_sol(matriz_res)){
 		/* Si no hay solucion devuelvo -1 */
@@ -38,39 +95,12 @@ tuple<int, int, int> caballos_salvajes(int n, int k, pair<int, int> posiciones_c
 		return res;
 	}
 	
-	tuple<int, int, int> res = buscar_minimo(matriz_res);
+	tuple<int, int, int> res = buscar_minimo(matriz_res,n);
 	
 	return res;
 	
 }
 
-
-/* Esta funcion modifica la primer matriz, dejando la suma de todas las
-matrices pasadas como parametro */
-void sumar_matrices(int matriz_res[][]&, int matrices_caballos[][][]&){
-	
-	//inicializo el resultado en 0
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			matriz_res[i][j] = 0;		
-		}
-	}
-	/* 
-	 Sumo las matrices considerando si alguno de los valores a sumar
-	 es -1, si es asi dejo -1, sino los sumo.
-	*/
-	for(int h = 0; h<k ; h++){
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				if(matrices_caballos[h][i][j] == -1 || matriz_res[i][j] == -1){
-					matriz_res[i][j] = -1;
-				} else {
-					matriz_res[i][j] = matriz_res[i][j] + matrices_caballos[h][i][j];
-				}
-			}
-		}
-	}
-}
 
 
 int main(int argc, char** argv){
@@ -90,7 +120,7 @@ int main(int argc, char** argv){
 	}
 	
 	//tuple<int,int,int> res ;
-	auto res = caballos_salvajes(n,k,posiciones_caballos));
+	auto res = caballos_salvajes(n,k,posiciones_caballos);
 	
 	if(get<0>(res) == -1){
 		cout << "NO" << endl;
@@ -101,7 +131,4 @@ int main(int argc, char** argv){
 	
 	return 0;
 	}
-
-
-
 
