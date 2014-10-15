2957ca908bd980fe90439adeca1afd5bfea5a3f2
Flower <flower@Power.(none)>
2014-10-14 21:16:47 -0300

    a_testear_ej1

--- tp2/test/ej1/testear_prom.sh
+++ tp2/test/ej1/testear_prom.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 # PARAMS: cantidad de tablones ini,cantidad de tablones hasta,tablones incremento, capacidad de salto, cantidad de iteraciones
 CANT_TABLONES=$1
-END=$3
-INC=$4
-ITERACIONES=$5
+END=$2
+INC=$3
+ITERACIONES=$4
 
 while ((CANT_TABLONES<END)); do
-        ./prom $CAPACIDAD_SALTO $CANT_TABLONES | ./ej1test $ITERACIONES
+        ./prom $CANT_TABLONES | ./ej1test $ITERACIONES
         let CANT_TABLONES=$CANT_TABLONES+$INC
 done
 
