88b5d0c52cf952336c3c9f47316b676c8bbfe37c
Martin Caravario <macaravario@dc.uba.ar>
2014-11-28 19:04:31 -0300

    parece funcionar el codigo del goloso

--- tp3/src/goloso.cpp
+++ tp3/src/goloso.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define MAX_INT 2147483647
+
 struct particion{
 	list<int> elementos;
 	int peso;
@@ -21,29 +23,8 @@ list<tuple<int,int,int> >::iterator elegir_mayor(list<tuple<int,int,int> >& aris
 		}		
 	}
 	
-	/*
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			if(get<0>(mz_ady[i][j]) && get<1>(mz_ady[i][j]) > max){
-				max = get<1>(mz_ady[i][j]);
-				get<0>(res) = i;
-				get<1>(res) = j;
-			}
-		}
-	}
-	*/
-
-
 	return res;
 
-}
-/* Devuelve la suma total de pesos */
-int suma_total(vector<particion>& partes, int k){
-	int res = 0;
-	for (int i = 0; i<k ; i++){
-		res += partes[i].peso;
-	}
-	return res;	
 }
 
 /* Devuelve el peso asociado del nodo v, al conjunto i */
@@ -57,73 +38,30 @@ int peso_asociado(int v, vector<particion>& partes, int i,  vector<vector<int> >
 	return res;
 }
 
-/* Ubica los vertices u y v en el conjunto que menos peso sume al agregarlos */
-void ubicar_en_menor(int u, int v, vector<particion>& partes, int k, int pesomax, vector<vector<int> >& mz_ady, vector<int>& vistos){
+/* Ubica el vertice u en el conjunto que menos peso sume al agregarlo */
+void ubicar_en_menor(int u, vector<particion>& partes, int k, vector<vector<int> >& mz_ady, vector<int>& vistos){
 	
-	int max = pesomax;
-	/*aca guardo el numero de particion */
-	int res;
+	int n_part;
+	int min = MAX_INT;
 	
 	/* Trato de ubicar el vertice u, si es que no lo ubique previamente */
-	if(vistos[u] != -1){
+	if(vistos[u] == -1){
 		for (int i = 0; i<k; i++){																						//O(K)*
-			bool a = partes[i].elementos.empty();
-			partes[i].elementos.push_front(u);
-			int peso_viejo = partes[i].peso;
-			partes[i].peso = peso_asociado(u, partes, i, mz_ady);							//O(N)
-			if(suma_total(partes,k) < max && i != vistos[v]){										//O(K)
-				res = i;
-				max = suma_total(partes,k);
+			int peso_nuevo = peso_asociado(u, partes, i, mz_ady);							//O(N)
+			if (peso_nuevo < min) {
+				min = peso_nuevo;
+				n_part = i;
 			}
-			partes[i].elementos.pop_front();
-			partes[i].peso = peso_viejo;						//O(N)
 		}
-	
-		/* En res tengo el numero de particion donde menos peso suma, agrego el
-		 * vertice a la particion y luego le agrego el peso
-		*/
-		partes[res].elementos.push_front(u);
-		partes[res].peso = peso_asociado(u, partes, res, mz_ady);
+		/**
+		 * En n_part tengo el conjunto donde va u.
+		 **/
+		partes[n_part].elementos.push_front(u);
+		partes[n_part].peso += min;
 		/* Actualizo el arreglo donde tengo los vertices que ya ubique */
-		vistos[u] = res;
-	}else{
-		res = vistos[u];
+		vistos[u] = n_part;
+		cout << "ubique el nodo " << u << "en la particion " << n_part << endl;
 	}
-
-	cout << "ubique el nodo " << u << "en la particion " << res << endl;
-	int res_v;
-	
-	/* Trato de ubicar el segundo vertice(v) en una particion distinta que la
-	 * del primer vertice, si es que todavia no lo ubique */
-	if(vistos[v] != -1){
-		for (int j = 0; j < k; j++){	
-			partes[j].elementos.push_front(v);
-			int peso_viejo = partes[j].peso; 
-			partes[j].peso = peso_asociado(v, partes, j, mz_ady);
-			if(suma_total(partes,k) < max && j != res){
-				res_v = j;
-				max = suma_total(partes,k);
-			}
-			partes[j].elementos.pop_front();
-			partes[j].peso = peso_viejo;
-		}
-		partes[res_v].elementos.push_front(v);
-		partes[res_v].peso = peso_asociado(v, partes, res_v, mz_ady);
-		vistos[v] = res_v;
-	}
-
-	cout << "ubique el nodo " << v << "en la particion " << res_v << endl;
-}
-
-
-/* Calcula el peso maximo que puede tener una particion, tener en cuenta que en la lista de aristas
- * no debe haber aristas repetidas */
-int calcular_peso_max(list<tuple<int,int,int> >& aristas){
-	int res = 0;
-	for (auto it = aristas.begin(); it != aristas.end(); it++){
-		res += get<2>(*it);
-	}
-	return res;
 }
 
 
@@ -132,18 +70,18 @@ int calcular_peso_max(list<tuple<int,int,int> >& aristas){
  * matriz de adyacencias : si hay arista entre u y v se guarda el peso de la arista, sino -1 
  * vector(particion) partes : vector donde se almacenara la respuesta, en cada particion se guardan los elementos y el peso respectivo de la particion.
  * lista(tupla(int.int.int)) aristas : representa todas las aristas del grafo, los primeros dos ints indican los nodos sobre los que incide y el tercer int, es el peso.
- * vector(int) vistos: la posicion i indica en que particion se encuentra el nodo i. si no fue ubicado en ninguna particion tiene el valor -1
+ * vector(int) vistos: la posicion i indica en que particion se encuentra el nodo i. si no fue ubicado en ninguna particion tiene el valor -2
 */
 
 vector<particion> heuristica_golosa(vector<vector<int> >& mz_ady, vector<particion>& partes, list<tuple<int,int,int> >& aristas, int k, vector<int>& vistos){
-	int pesomax = calcular_peso_max(aristas);
 				
 	while(!aristas.empty()){    																						//O(M) *
 		/* Elijo arista de mayor peso */
 		auto arista = elegir_mayor(aristas);																	//O(M)
 		int u = get<0>(*arista);
 		int v = get<1>(*arista);
-		ubicar_en_menor(u, v, partes, k, pesomax, mz_ady, vistos);						//O(K*K + K*N)
+		ubicar_en_menor(u, partes, k, mz_ady, vistos);						//O(K*K + K*N)
+		ubicar_en_menor(v, partes, k, mz_ady, vistos);						//O(K*K + K*N)
 		/* Una vez analizada la arista la saco de la matriz de adyacencia y de
 		 * mi lista de aristas restantes */
 		mz_ady[u][v] = -1;	
@@ -188,7 +126,7 @@ int main(){
 	}
 	/* Inicializo los no vistos nunca en -2 */
 	for(int i = 0; i<n ; i++){
-		vistos[i] = -2;
+		vistos[i] = -1;
 	}
 
 	/* Funcion que resuelve el ejercicio */
@@ -197,35 +135,9 @@ int main(){
 	
 	/* Preparacion del resultado */
 
-
-	//	vector<int> resultado_final;
-	//	resultado_final.reserve(n);
-	for(int i = 0; i < k; i++){
-		for (auto it = res[i].elementos.begin(); it != res[i].elementos.end(); it++){
-			cout << *it << " ";
-			//resultado_final[*it] = i;
-		}
-	}
-/*
 	for(int i = 0; i < n ; i++){
-		cout << resultado_final[i] << " "; 
+		cout << vistos[i] << " "; 
 	}
-	*/
+
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
