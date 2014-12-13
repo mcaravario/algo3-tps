4d853abebce0c0c7f29a09a62240719c4935ae68
fhosen <fhosen@hotmail.com>
2014-12-13 13:01:01 -0300

    Se agrega un experimento y se reestructura un poco la carpeta de test.

---
+++ tp3/test/experimentos/exacto/master.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+# Script encargado de correr el experimento
+# Llama a los scrips que miden tiempos.
+
+# Sin poda
+./exacto_run_n.sh 2 13 5 6 28 1 < ek2.out
+./exacto_run_n.sh 2 13 5 6 28 1 < ek3.out
+./exacto_run_n.sh 2 13 5 6 28 1 < ek4.out
+
+
+# Con poda
+./pexacto_run_n.sh 2 13 5 6 28 1 < pek2.out
+./pexacto_run_n.sh 2 13 5 6 28 1 < pek3.out
+./pexacto_run_n.sh 2 13 5 6 28 1 < pek4.out
---
+++ tp3/test/experimentos/exacto/pexacto_run_n.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+# Itera sobre la cantidad de conjuntos.
+# PARAMS: k (cantidad de conjuntos), s (semilla), i (iteraciones), d (cantidad inicial de nodos), h (cantidad final de nodos), inc (incremento)
+k=$1
+s=$2
+i=$3
+d=$4
+h=$5
+inc=$6
+
+while (($d<$h)); do
+				../../bin/generador_grafo $d $k $s | ../../bin/pexacto
+        let d=$d+$inc
+
+done
