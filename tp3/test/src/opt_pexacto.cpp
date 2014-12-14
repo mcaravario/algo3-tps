5ea6c179aaa1d83559199d8d68658718b17d351d
Martin Caravario <macaravario@dc.uba.ar>
2014-12-14 19:43:05 -0300

    Agregado el generador de casos malos y los cpp modificados para que impriman los pesos del goloso y exacto

---
+++ tp3/test/src/opt_pexacto.cpp
@@ -0,0 +1,27 @@
+#include "exacto.h"
+
+int main(){
+	int n,m,k,u,v,w;
+	cin >> n;
+	cin >> m;
+	cin >> k;
+
+	list<arista> aristas;
+	
+	/* Creo la lista de aristas.  */
+	for (int i = 0; i < m; i++){
+		cin >> u;
+		cin >> v;
+		cin >> w;
+		u--;
+		v--;
+	 	aristas.push_back(arista(u, v, w));
+	}
+
+	/* Llamo a la funcion que resuelve el ejercicio. */
+	cout << peso_de_exacto_poda(aristas, n, k); 
+
+	return 0;
+}
+
+	
---
+++ tp3/test/src/opt_pgoloso.cpp
@@ -0,0 +1,27 @@
+#include "goloso.h"
+
+int main(){
+	int n,m,k,u,v,w;
+	cin >> n;
+	cin >> m;
+	cin >> k;
+
+	list<arista> aristas;
+
+	/* Creo la lista de aristas */
+	for (int i = 0; i < m; i++){
+		cin >> u;
+		cin >> v;
+		cin >> w;
+		u--;
+		v--;
+		aristas.push_back(arista(u,v,w));
+	}
+
+	/* Llamo a la función que resuelve el ejercicio. */
+  cout << peso_de_golosa_ordenada(aristas, n, k);
+
+	/* Muestro el resultado por salida estandar. */
+
+	return 0;
+}
