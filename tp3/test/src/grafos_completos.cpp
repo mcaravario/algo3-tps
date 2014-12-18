55106440cbde86c14c6b1de9b6d34af94ad0ea6a
Federico Hosen <fhosen@hotmail.com>
2014-12-18 16:45:25 -0300

    Modificado el generador de grafos completos, se le pasa n y como tercer parametro k

--- tp3/test/src/grafos_completos.cpp
+++ tp3/test/src/grafos_completos.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
 	if(argc < 2) cout << "faltan argumentos" << endl;  
 	// Recibe como parametros el n (Cant productos) y el M (umbral).
   int n = atoi(argv[1]);
-  int m = atoi(argv[2]);      
+  int m = (n*(n-1))/2;      
   int k = atoi(argv[3]);      
   cout << n << " " << m << " " << k << endl;
   generar_valida(n,m);
@@ -19,8 +19,8 @@ int main(int argc, char** argv){
 void generar_valida(int n,int m){
   for(int j = 1; j <= m; j++){
 			for (int i = (j+1); i<=n; i++){
-			int arista = rand()% 60 + 1;
-			cout << j << " " <<  i << " " << arista << endl;
+				int arista = rand()% 60 + 1;
+				cout << j << " " <<  i << " " << arista << endl;
 			}
 	}
 }
