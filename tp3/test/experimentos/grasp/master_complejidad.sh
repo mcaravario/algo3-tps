2e5364b60c49571427e0f7bd6aa3283eea14f227
Federico Hosen <fhosen@hotmail.com>
2014-12-18 23:36:20 -0300

    Complejidad grasp, resultados experimentos.

--- tp3/test/experimentos/grasp/master_complejidad.sh
+++ tp3/test/experimentos/grasp/master_complejidad.sh
@@ -4,9 +4,9 @@
 # Se usa un grafo completo con n = 200,  k = 10.
 # El modo de uso de goloso es 1, y el cant_elegir es 10.
 
-# K-iteraciones fijas, irán de 1 a 1000, incrementando de a 1
-./complejidad_iteraciones.sh 0 200 10 13 1 1000 1 1 10 > grasp.compl.0.out
+# K-iteraciones fijas, irán de 1 a 300, incrementando de a 1
+#./complejidad_iteraciones.sh 0 200 10 13 1 300 1 1 10 > grasp.compl.0.out
 
 
-# K-iteraciones sin mejoras, irán de 1 a 1000, incrementando de a 1
-./complejidad_iteraciones.sh 1 200 10 13 1 1000 1 1 10 > grasp.compl.1.out
+# K-iteraciones sin mejoras, irán de 1 a 300, incrementando de a 1
+./complejidad_iteraciones.sh 1 200 10 13 1 300 1 1 10 > grasp.compl.1.out
