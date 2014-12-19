7e1a23864017bd524c7047c77489b333325f19f2
Flower <flower@Power.(none)>
2014-12-18 23:58:44 -0300

    test local

--- tp3/test/experimentos/busquedalocal/complejidad/correccion/busqueda_local_1opt_run_k.sh
+++ tp3/test/experimentos/busquedalocal/complejidad/correccion/busqueda_local_1opt_run_k.sh
@@ -9,10 +9,7 @@ h=$5
 inc=$6
 
 while (($k<$h)); do
-	let CUENTA1=$d\*$d
-	let CUENTA2=$CUENTA1-$d
-	let ARISTAS=$CUENTA2/2
-	../../../../bin/grafos_completos $d $ARISTAS $k | ../../../../bin/busqueda_local_1opt_aleatorio $s $i 
+	../../../../bin/grafos_completos $d $k $s | ../../../../bin/busqueda_local_1opt_aleatorio $s $i 
   let k=$k+$inc
 
 done
